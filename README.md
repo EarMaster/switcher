c23133da04ac5760e45e49b685d7af10568041a9
Nico Wiedemann <nico.wiedemann@gmx.de>
2012-11-26 10:44:32 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-switcher
-========
+switcher provides a similar syntax to a switch statement in JavaScript but with RegExp as cases.
 
-switcher provides a similar syntax to a switch statement in JavaScript but with RegExp as cases
+# How to use
+I tried to mimic the syntax of a switch statement as good as possible. The following example shows how to match against the UserAgent string to detect iOS devices.
+
+```javascript
+switcher(navigator.userAgent, {
+	'/i(OS|Pad|Phone|Pod)/': function (scope, expression) {
+		console.log('iOS device! '+scope+' did match '+expression);
+	},
+	'': function () {
+		console.log('any device');
+	}
+});
+```
+
+Note that the Regular Expressions are written as Strings as JavaScript doesn't allow RegExp objects as keys. The default case is represented by an empty string as this one matches everything. You should always put the default case at last as with ``breakMode`` set to ``true`` no cases after the default one could be reached.
+
+# Options
+``breakMode``  
+When set to ``true`` (which is default) switcher will stop testing after the first match. When set to ``false`` switcher will continue testing (but note that changes that may have been made by previous matches can be overwritten ‒ *especially if you have a default case at the end*).
+
+# License
+[MIT license](http://opensource.org/licenses/MIT)
---
+++ switcher.js
@@ -0,0 +1,53 @@
+/*! 
+ * switcher 1.0 (c) 2012 Nico Wiedemann - MIT license
+ */
+!function (definition) {
+	if (typeof define==="function" && define.amd)
+		define(definition);
+	else
+		self.switcher = definition();
+}(function () {
+	/*switcher(navigator.userAgent, {
+		'': function (scope) {
+			// default case (should be last with breakMode set to true!)
+		},
+		'/i(OS|Pad|Phone|Pod)/': function () {
+
+		}
+	})*/
+	'use strict';
+	/**
+	 * function to provide a similar syntax to a switch statement but with regex as cases
+	 * @param {String} scope string on which the regexes should be tested against
+	 * @param {Object} tests object containing regexes as keys and callback functions as values which will be called with the scope as first parameter if the regex match.
+	 * @param {Object} options object containing options like breakMode (see https://github.com/EarMaster/switcher for full list of options)
+	 * @return {Boolean} returns true if one or more tests matched
+	 */
+	var switcher = function (scope, tests, options) {
+		var defaultOptions = {
+			breakMode: true // if set to true switcher will stop after the first match
+		};
+		if (!options)
+			options = defaultOptions;
+		else
+			for (var option in defaultOptions)
+				if (!options.hasOwnProperty(option))
+					options[option] = defaultOptions[option];
+		var matched = false;
+		for (var test in tests) {
+			test = {
+				fullExpression: test,
+				expression: test.substr(0,1)=='/'?test.substr(1, test.lastIndexOf('/')-1):test,
+				flags: test.substr(0,1)=='/'?test.substr(test.lastIndexOf('/')+1):''
+			};
+			if (new RegExp(test.expression, test.flags).test(scope)) {
+				matched = true;
+				tests[test.fullExpression](scope, test.fullExpression);
+				if (options.breakMode)
+					break;
+			}
+		}
+		return matched;
+	};
+	return switcher;
+});
